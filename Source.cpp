24ba3a01c70b670cbbee0a4109c7b75dc149ff30
David Poss <davidp525@gmail.com>
2017-09-29 18:47:01 -0700

    Still need to fix real FSM

--- Source.cpp
+++ Source.cpp
@@ -13,6 +13,7 @@
 #include <list>
 
 #define ERROR_STATE -1
+#define DEFAULT_STATE 0
 
 #define NUM_KEYWORDS 13
 std::string keywords[NUM_KEYWORDS] = {
@@ -39,31 +40,31 @@ std::string Tokens[NUM_TOKENS] = {
 
 /* Transition tables */
 const int idTable[4][2] = {
-	/* Inputs		l	 #       /*
-	/* State 0 */	2,	-1,
-	/* State 1 */	2,	 3,
-	/* State 2 */	4,	-1,
-	/* State 3 */	4,	 3,
+	/* Inputs		l	 #      /*
+	/* State 0 */	1,	-1,		/* start with # */
+	/* State 1 */	1,	 2,
+	/* State 2 */	3,	-1,		/* no consecutive #s */
+	/* State 3 */	3,	 2,
 	/* -1 = empty set			*/
 };
-const int idAcceptStates[] = { idTable[2][0], idTable[3][0] };
+const int idAcceptStates[] = { 1, 3 };
 
 const int intTable[2][1] = {
 	/* Inputs		d */
-	/* State 1 */	2,
-	/* State 2 */	2
+	/* State 1 */	1,
+	/* State 2 */	1
 };
-const int intAcceptStates[] = { 2 };
+const int intAcceptStates[] = { 1 };
 
 const int realTable[4][2] = {
 	/* Inputs		d	'.'		*/
-	/* State 1 */	2,	-1,
-	/* State 2 */	2,	 3,
-	/* State 3 */	4,	-1,
-	/* State 4 */	4,	-1
+	/* State 1 */	1,	-1,
+	/* State 2 */	2,	 2,
+	/* State 3 */	3,	-1,
+	/* State 4 */	3,	-1
 	/* -1 = empty set			*/
 };
-const int realAcceptStates[] = { 4 };
+const int realAcceptStates[] = { 3 };
 
 bool isKeyword(std::string lexeme) {
 	for (int i = 0; i < NUM_KEYWORDS; i++) {
@@ -76,7 +77,6 @@ bool isKeyword(std::string lexeme) {
 bool isSeparator(std::string str) {
 	for (int i = 0; i < NUM_SEPARATORS; i++) {
 		if (separators[i] == str) {
-			std::cout << "found separator:   " << str << std::endl;
 			return true;
 		}
 	}
@@ -85,7 +85,6 @@ bool isSeparator(std::string str) {
 bool isOperator(std::string str) {
 	for (int i = 0; i < NUM_OPERATORS; i++) {
 		if (operators[i] == str) {
-			std::cout << "found operator:    " << str << std::endl;
 			return true;
 		}
 	}
@@ -95,26 +94,25 @@ bool isSpace(std::string str) {
 	return str == " ";
 }
 bool isDigit(std::string str) {
-	for (size_t i = 0; i < str.length(); i++) {
-		if (!isdigit(str[i])) {
-			return false;
-		}
-	}
-	return true;
+	return isdigit(str[0]);
 }
 bool isAlphaOrPound(std::string str) {
 	return ((isalpha(str[0])) || str[0] == '#');
 }
 
 enum TokenType {
-	Integer = 1, Real = 2,
-	Identifier = 3, Operator = 4,
-	Keyword = 5, Separator = 6
+	Integer = 1,
+	Real = 2,
+	Identifier = 3,
+	Operator = 4,
+	Keyword = 5,
+	Separator = 6,
+	Unknown = 7
 };
 
 class Token {
 public:
-	Token() : state_(0), accept_(false), token_(""), type_(Integer) { }
+	Token() : state_(0), accept_(false), token_(""), type_(Unknown) { }
 
 	Token(int state, bool accept, std::string token, TokenType type)
 		: state_(state), accept_(accept), token_(token), type_(type) { }
@@ -122,37 +120,63 @@ public:
 	void updateType(TokenType type) { type_ = type; }
 	void updateToken(std::string val) { token_ += val; }
 
-	bool isReal() { return (accept_ && type_ == Real); }
-	bool isInteger() { return (accept_ && type_ == Integer); }
-	bool isIdentifier() { return (accept_ && type_ == Identifier); }
-
+	bool isReal() { return (type_ == Real); }
+	bool isInteger() { return (type_ == Integer); }
+	bool isIdentifier() { return (type_ == Identifier); }
 
 	void clear() {
 		token_ = "";
-		type_ = Integer;
+		type_ = Unknown;
 		state_ = 0;
 		accept_ = false;
 	}
 
 	void updateState(std::string input) {
 		if (type_ == Integer && isDigit(input)) {
-			/* No lookup needed, integers are always in a final state */
-			state_ = 2;
+			std::cout << "input is: " << input << std::endl;
+			state_ = intAcceptStates[0];
 			accept_ = true;
 		}
-		if (type_ == Real && (isDigit(input) || input == ".")) {
-			if (input == ".") { state_ = realTable[state_ - 1][1]; }
-			else { state_ = realTable[state_ - 1][0]; }
-			accept_ = (state_ == realAcceptStates[0]);
-		}
 
+		if (type_ == Real && (isDigit(input)) || input == ".") {
+			if (state_ >= 0) {
+				if (input == ".") {
+					std::cout << "found a period!  " << state_ << std::endl << std::endl;
+					state_ = realTable[state_][1];
+				}
+				else {
+					state_ = realTable[state_][0];
+				}
+				accept_ = (state_ == realAcceptStates[0]);
+			}
+		}
+		else {
+			accept_ = false;
+		}
+	
+		
+
+
+		/* Identifier */
 		if (type_ == Identifier && isAlphaOrPound(input)) {
-			if (input == "#") { state_ = idTable[state_ - 1][1]; }
-			else { state_ = idTable[state_ - 1][0]; }
-			accept_ = (state_ == idAcceptStates[0] || state_ == idAcceptStates[1]);
+			if (state_ >= 0) {
+				if (input == "#") {
+					state_ = idTable[state_][1];
+				}
+				else if (isalpha(input[0])) {
+					state_ = idTable[state_][0];
+				}
+				accept_ = (state_ == idAcceptStates[0] || state_ == idAcceptStates[1]);
+			}
+			else {
+				accept_ = false;
+			}
 		}
 	} /* end updateState */
 
+	void updateState(int state) {
+		state_ = state;
+	}
 
 	friend std::ostream& operator<<(std::ostream& os, const Token& t) {
 		os << t.token_ << "\t\t" << t.type_ << std::endl;
@@ -162,12 +186,7 @@ public:
 	bool isAccepted() { return accept_; }
 	TokenType getType() { return type_; }
 	std::string getToken() { return token_; }
-
-	/* TODO
-	Add function to see if Token is in an accept state according to table_
-	Add function to pass all of current token through a machine
-	*/
-
+	int getState() { return state_; }
 
 private:
 	int			state_;
@@ -180,13 +199,12 @@ struct Pair {
 	Pair() : val1("empty"), val2("empty") {}
 	Pair(std::string l, std::string t) : val1(l), val2(t) {}
 
-	std::string val1;
-	std::string val2;
-
 	friend std::ostream& operator<<(std::ostream& os, Pair P) {
-		os << "Token: " << P.val1 << "      Type: " << P.val2;
+		os << "Token: " << P.val1 << "\t\t Type: " << P.val2;
 		return os;
 	}
+
+	std::string val1, val2;
 };
 
 class Lexer {
@@ -209,7 +227,11 @@ public:
 		}
 	}
 	/* Function to add a lexeme to the list */
-	void addLexeme(Token lexeme) {
+	void addLexeme(Token& lexeme) {
+		if (!lexeme.isAccepted()) {
+			std::cout << "Token " << lexeme.getToken() << " not accepted " << std::endl;
+			return;
+		}
 		switch (lexeme.getType()) {
 		case(Integer):
 			lexemes_.push_back(Pair(lexeme.getToken(), "integer"));
@@ -232,68 +254,62 @@ public:
 		default:
 			return;
 		}
+		lexeme.clear();
 		return;
 	}
 
-
-
-	/* Main lexer function */
+	/*		Main lexer function		*/
 	void getTokens(std::string inputCode) {
 		/* Read character from file one at a time, until eof
 		std::fstream inputFile;
 		inputFile.open(filename, 'r');
 		while (inputFile.get(c)) { */
 
-		/* currentLexeme is the full string token that we're building*/
-		Token currentLexeme(0, false, "", Integer);
+		Token currentLexeme;
 
-		for (size_t i = 0; i < inputCode.length(); i++)
-		{
-			char c = inputCode[i];
-			std::string input;
-			input.push_back(c);
-			//* Start of new token, get the type it might be */
-			if (currentLexeme.getToken().length() == 0) {
-				currentLexeme.updateType(charToTokenType(input));
-				currentLexeme.updateToken(input);
-			}
+		for (std::string::iterator i = inputCode.begin(); i != inputCode.end(); i++) {
+			std::string input = std::string(1, *i);
 
-			std::cout << "i == " << i << ":  " << currentLexeme << std::endl;
-			// Convert char to string, input serves as a single character string
-			
-			if (currentLexeme.getType() == Integer && charToTokenType(input) == Real) {
-				// We changed token types from integer to real
-				// feed currentToken through real FSM 
+			if (charToTokenType(input) == Real && currentLexeme.getType() == Integer) {
+				currentLexeme.updateState(realTable[2][0]);
 				currentLexeme.updateType(Real);
 			}
+			else if (charToTokenType(input) == Integer && currentLexeme.getType() != Real) {
+				currentLexeme.updateType(Integer);
+				std::cout << currentLexeme.getToken() << std::endl;
+			}
 
-			//// See if input terminates a token
+			// See if input terminates a token
 			if (isSeparator(input) || isOperator(input) || isSpace(input)) {
 				if (isKeyword(currentLexeme.getToken())) {
 					currentLexeme.updateType(Keyword);
 					addLexeme(currentLexeme);
 				}
+				if (isSpace(input) && currentLexeme.getToken() != "") {
+					addLexeme(currentLexeme);
+				}
 				if (currentLexeme.isIdentifier()) {
 					addLexeme(currentLexeme);
+				}
+				if (currentLexeme.isInteger()) { 
+					std::cout << currentLexeme.getToken() << " should be an integer\n";
+					addLexeme(currentLexeme); 
 				}
 				if (currentLexeme.isReal()) {
 					addLexeme(currentLexeme);
 				}
-				if (currentLexeme.isInteger()) {
-					addLexeme(currentLexeme);
+				if (isSeparator(input)) {
+					addLexeme(Token(0, true, input, Separator));
 				}
-				if (isSeparator(currentLexeme.getToken())) {
-					addLexeme(currentLexeme);
-				}
-				if (isOperator(currentLexeme.getToken())) {
-					if (isOperator(currentLexeme.getToken() + input)) {
-						currentLexeme.updateToken(input);
-						addLexeme(currentLexeme);
+				if (isOperator(input)) {
+					/* See if next token is also an operator, like <= */
+					if (isOperator(input + *(i + 1))) {
+						addLexeme(Token(0, true, input + *(i + 1), Operator));
+						i++;
 					}
-					else {
-						addLexeme(currentLexeme);
-					}
+					else { addLexeme(Token(0, true, input, Operator)); }
 				}
+
 				currentLexeme.clear();
 			}
 			// Don't want to make currentLexeme = " "
@@ -301,44 +317,28 @@ public:
 			//	currentLexeme.clear();
 			//	currentLexeme.updateToken(input);
 			//}
-			else {
-				// Get the next input in inputCode
-				//currentLexeme.clear();
-				currentLexeme.updateToken(input);
-				currentLexeme.updateType(charToTokenType(input));
+			else { /* input doesn't terminate a token */
+				if (isDigit(input) && currentLexeme.getType() == Integer) {
+					currentLexeme.updateToken(input);
+					std::cout << "\ninteger: " << currentLexeme.getToken() << std::endl;
+					currentLexeme.updateState(input);
+						std::cout << "11 should be lkjhlkjaccepted\n\n";
+					if (currentLexeme.isAccepted()) {
+					}
+				}
+				else if (input == "." || (isDigit(input) && currentLexeme.getType() == Real)) {
+					currentLexeme.updateToken(input);
+					currentLexeme.updateState(input);
+				}
+				// We are reading an identifier or keyword
+				else if (isAlphaOrPound(input)) {
+					currentLexeme.updateToken(input);
+					currentLexeme.updateType(Identifier);
+					currentLexeme.updateState(input);
+				}
 			}
-			///* we are reading an integer */
-			//if (isDigit(input)) {
-
-			//}
-
-			///* We are reading an identifier or keyword */
-			//if (isAlphaOrPound(input)) {
-
-			//}
-
-			// if input's token possibilities match c's (i.e input is a letter and c has letters
-			// if input is an inter and c is an identifier/keyword, then we break			
-			
-			//currentLexeme.updateToken(input);
-			//currentLexeme.updateState(input);
-						
-			//
-			//if (isDigit(c) && !isReal) {
-			//	// state = Integer.nextState()
-			//}
-			//// Check if c builds a real
-			//if (isDigit(c) && isReal) {
-			//	// runStackThroughReals()
-			//}
-			//if (c == ".") {
-			//	isReal = true;
-			//}
-
-			input.clear();
 		} /* end for loop */
 	} /* end function */
-
 
 	void printLexemes() {
 		std::cout << "\nPrinting lexemes\n";
@@ -353,19 +353,8 @@ private:
 
 
 int main() {
-	/*
-		std::list<Pair> p;
-		p.push_back(Pair("13454", "integer"));
-		p.push_back(Pair("41233", "integer"));
-		p.push_back(Pair("123.331", "real"));
-		p.push_back(Pair("while", "keyword"));
 
-		for (std::list<Pair>::iterator it = p.begin(); it != p.end(); it++) {
-			std::cout << *it << std::endl;
-		}
-	*/
-
-	std::string inputCode = "while (fahr#x < upper) a = 23.00 ";
+	std::string inputCode = "while (fahr#x <= upper) a = 23.00  f##f #ff f#f#ff 11 43.34.32 ";
 	Token token;
 	Lexer lexer;
 	lexer.getTokens(inputCode);
