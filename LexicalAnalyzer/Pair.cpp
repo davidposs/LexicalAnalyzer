0a2befc8ea8526b6b89cb7c13047c26bdaf2c3f8
David Poss <davidposs@csu.fullerton.edu>
2017-09-30 19:49:44 -0700

    Added functionality for running multiple files

---
+++ LexicalAnalyzer/Pair.cpp
@@ -0,0 +1,18 @@
+/******************************************************************************
+* File:    Pair.cpp
+* Authors: David Poss, Douglas Galm
+*
+* Usage:   To create lexeme and token-type pairs that can be easily printed,
+*          modified, and inserted into a list.
+* 
+******************************************************************************/
+
+#include "Pair.h"
+
+Pair::Pair() : val1(""), val2("") {}
+Pair::Pair(std::string l, std::string t) : val1(l), val2(t) {}
+
+std::ostream& operator<<(std::ostream& os, Pair P) {
+	os << "Token: " << P.val1 << "\t\t Type: " << P.val2;
+	return os;
+}
